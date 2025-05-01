083909391f75aca5d54d7e6cb3645578291c554d
Vinh Huynh <vinhkhanghuynh03@gmail.com>
2025-05-01 15:30:56 -0500

    second commit

--- R/mysamplefuntion.R
+++ R/mysamplefuntion.R
@@ -7,12 +7,15 @@
 #' @export
 myf=function(n, iter=10,time=0.5){
   for( i in 1:iter){
+    #loonig sample to fully randomize
     s=sample(1:10,n,replace=TRUE)
     sf=factor(s,levels=1:10)
+    # barplot
     barplot(table(sf)/n,beside=TRUE,col=rainbow(10),
             main=paste("Example sample()", " iteration ", i, " n= ", n,sep="") ,
             ylim=c(0,0.2)
     )
+    # timing
     Sys.sleep(time)
   }
 }
